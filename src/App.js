6642219a74f169465f82a516f679678c61d0ccd7
imene joubeli <joubeliimen@gmail.com>
2021-05-12 01:33:47 +0100

    modify props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import Profile from './profile/profile.js'
+import './style.css'
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 function App() {
-  let profile=[{
-    Name:'Imen',
-    bio:`My name is Imen Joubeli and I am a full-stack Web
+  const alertName = (myname) => {
+    alert(`Hello I am ${myname}`);
+  };
+  let profile = [{
+    Name: 'Imen Joubeli (Bou A li)',
+    /* img:'https://holocaustmusic.ort.org/typo3conf/ext/ne_hm_t3template/Resources/Public/Images/loader.gif', */
+    img:'https://static.wixstatic.com/media/7cfced_b653aba696ce4833a9b08d26f8ecfb19~mv2.gif',
+    bio: `My name is Imen Joubeli and I am a full-stack Web
      Application Developer and Software Developer, currently living in Tunisia. 
      I have a Bachelor degree in Computer Science from the higher institute of 
      economical and commercial science of tunis, and a master degree in Data Science for Business.
      My primary focus and inspiration for my studies is Web Development`,
-     profession:'Full-Stack-Web developer',
-}]
+    profession: 'Full-Stack web developer',
+  }]
   return (
-    <div>
-      <Profile profile={profile}>
-      <h2>hello word</h2>
-      <p>Hi ...</p>
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Blog</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">Academic profile</Nav.Link>
+            <NavDropdown title="Contact" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Contact1</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Contact2</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Contact3</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Social Media</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      
+      <Profile profile={profile} alertName={alertName}>
+        <p className='copyright'> © 2021 Copyright</p>
       </Profile>
-    </div>
+    </>
   );
 }
+
+
 
 export default App;
--- src/profile/profile.js
+++ src/profile/profile.js
@@ -1,18 +1,32 @@
 import React from 'react'
+import {Button } from 'react-bootstrap'
+import PropTypes from 'prop-types'
 
-const Profile = ({ profile, children }) => {
+
+const Profile = ({profile, children, alertName }) => {
     return (
-        <div>
+        <>
+        <div className='cimg'>
+        
+        <img className='img' src={profile[0].img} alt='image1'></img>
+        <Button  onClick={()=>alertName(profile[0].Name)}>Join Me On Instagram</Button>{' '}
+        </div>
+        <div className='profile'>
             <h1>{profile[0].Name}</h1>
-            <h2>{profile[0].bio}</h2>
-            <p>{profile[0].profession}</p>
+            <p style={{textAlign:'center'}}>{profile[0].bio}</p>
+            <p className='profession'>{profile[0].profession}</p>
             <div>
-                <span>Children</span>
                 {children}
             </div>
 
         </div>
+        </>
     )
 }
+
+Profile.propTypes = {
+     alertName: PropTypes.func.isRequired,
+  };
+
 
 export default Profile
